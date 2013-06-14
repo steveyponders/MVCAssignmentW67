94ced39801a5a3498db2a1bf6aa59055eef65f0b
Stephen Smuts <StephenS@bbd.co.za>
2013-06-14 11:57:47 +0200

    Final checkin.

---
+++ MVCAssignmentW67Sln/MVCAssignmentW67Sln/Areas/ContactDetails/Controllers/CreateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAssignmentW67Sln.Areas.ContactDetails.Controllers
+{
+    public class CreateController : Controller
+    {
+        //
+        // GET: /ContactDetails/Create/
+
+        public ActionResult Index(string xml)
+        {
+            ViewModels.ContactDetailsViewModel details = new ViewModels.ContactDetailsViewModel();
+            details = ViewModels.ContactDetailsViewModel.Deserialize(xml);
+
+            return View();
+        }
+
+    }
+}
