c6f9a0621443d983c1a79d6f0ccc5804f42e06f7
Stephen Smuts <StephenS@bbd.co.za>
2013-06-14 12:03:31 +0200

    Final checkin.

--- MVCAssignmentW67Sln/MVCAssignmentW67Sln/Areas/ContactDetails/Controllers/Management/ManagementController.cs
+++ MVCAssignmentW67Sln/MVCAssignmentW67Sln/Areas/ContactDetails/Controllers/Management/ManagementController.cs
@@ -13,9 +13,17 @@ namespace MVCAssignmentW67Sln.Areas.ContactDetails.Controllers
         public ActionResult Index()
         {
             ContactDetailsViewModel contactDetailsViewModel = new ContactDetailsViewModel();
-            contactDetailsViewModel.ContactDetails = new Models.ContactDetails();
-            contactDetailsViewModel.Address = new Models.Address();
-            contactDetailsViewModel.Phone = new Models.Phone();
+            contactDetailsViewModel.ContactDetails = new Models.ContactDetails()
+                {
+                    //CellPhone = new Models.Phone() {AreaCode = "082", CountryCode = "+27", PhoneNumber = "9991919"},
+                    Email = "test@ds.com",
+                    FirstName = "Steve",
+                    //HomeAddress = new Models.Address() { City = "a", Country = "SA", Line1 = "12 West str", Line2 = "Houghton", Line3 = "JHB", PostalCode = "2195", Province = "GAUTENG"},
+                    //HomePhone = new Models.Phone() {AreaCode = "082", CountryCode = "+27", PhoneNumber = "9991919"},
+                    LastName = "Smuts"
+                };
+            contactDetailsViewModel.Address = new Models.Address() { City = "a", Country = "SA", Line1 = "12 West str", Line2 = "Houghton", Line3 = "JHB", PostalCode = "2195", Province = "GAUTENG"};
+            contactDetailsViewModel.Phone = new Models.Phone() {AreaCode = "082", CountryCode = "+27", PhoneNumber = "9991919"};
             return View(contactDetailsViewModel);
         }        
 
